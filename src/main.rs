2654b6f78fa4961814b529d2c89eee100f672175
Tibor Benke <tibor.benke@balabit.com>
2015-08-12 16:41:47 +0200

    make it work :D
    Signed-off-by: Tibor Benke <tibor.benke@balabit.com>

--- src/main.rs
+++ src/main.rs
@@ -37,15 +37,26 @@ struct ConcreteReactor {
     map: BTreeMap<i32, Box<EventHandler<i32, Handler=i32>>>,
 }
 
+impl ConcreteReactor {
+    fn new(selector: Box<EventDemultiplexer<Event=i32>>) -> ConcreteReactor {
+        ConcreteReactor {
+            selector: selector,
+            map: BTreeMap::new()
+        }
+    }
+}
+
 impl Reactor for ConcreteReactor {
     type Event = i32;
     type Handler = i32;
     fn handle_events(&mut self) {
-        let event = self.selector.select();
+        loop {
+            let event = self.selector.select();
 
-        if let Some(handler) = self.map.get_mut(&event.handler()) {
-            println!("Event handled");
-            handler.handle_event(event);
+            if let Some(handler) = self.map.get_mut(&event.handler()) {
+                println!("Event handled");
+                handler.handle_event(event);
+            }
         }
     }
     fn register_handler(&mut self, handler: Box<EventHandler<Self::Event, Handler=Self::Handler>>) {
@@ -63,41 +74,33 @@ impl EventDemultiplexer for Demultiplexer {
     fn select(&mut self) -> Self::Event {
         thread::sleep_ms(500);
         self.0 += 1;
-        self.0
+        self.0 % 5
     }
 }
 
-struct A {
-    i: i32,
-    f: f32
-}
+struct A(i32);
 
 impl EventHandler<i32> for A {
     type Handler = i32;
     fn handle_event(&mut self, event: i32) {
-        println!("handled i32 event");
+        println!("handled i32 event: {}", event);
     }
     fn handler(&self) -> Self::Handler {
-        self.i
+        self.0
     }
 }
 
-impl EventHandler<f32> for A {
-    type Handler = f32;
-    fn handle_event(&mut self, event: f32) {
-        println!("handled f32 event");
-    }
-    fn handler(&self) -> Self::Handler {
-        self.f
-    }
+fn register_handlers(reactor: &mut Reactor<Event=i32, Handler=i32>) {
+    reactor.register_handler(Box::new(A(0)));
+    reactor.register_handler(Box::new(A(1)));
+    reactor.register_handler(Box::new(A(2)));
+    reactor.register_handler(Box::new(A(3)));
+    reactor.register_handler(Box::new(A(4)));
 }
 
 fn main() {
-    let mut a = A{i:1, f:3.14};
-    a.handle_event(2);
-    a.handle_event(2.0);
-    let mut b = Box::new(a);
-    b.handle_event(2);
-    b.handle_event(2.0);
-    println!("Hello, world!");
+    let dem = Demultiplexer(0);
+    let mut reactor = ConcreteReactor::new(Box::new(dem));
+    register_handlers(&mut reactor);
+    reactor.handle_events();
 }
